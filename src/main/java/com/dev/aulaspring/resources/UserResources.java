3adc40ade2c36520ee06b375ba80c2f0d21fa91c
andre <evaristoandre106@gmail.com>
2024-12-04 10:24:19 -0300

    user e resource criado

---
+++ src/main/java/com/dev/aulaspring/resources/UserResources.java
@@ -0,0 +1,18 @@
+package com.dev.aulaspring.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dev.aulaspring.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResources {
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User user = new User(1L, "andre", "ande@gmail.com", "1199999-9999", "1234");
+        return ResponseEntity.ok().body(user);
+    }
+}
